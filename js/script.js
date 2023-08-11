8d94f8e3d2c127ace9c566f980324593b10dab26
ritathakur29 <106301344+ritathakur29@users.noreply.github.com>
2023-08-11 12:54:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 const forms = document.querySelectorAll('.form-step');
 const prevButtons = document.querySelectorAll('.prev-step');
 const nextButtons = document.querySelectorAll('.next-step');
-var formInstance = document.getElementsByName('step'+currentStep);
-
 let currentStep = 0;
+var formInstance = document.getElementsByName('step'+currentStep);
 
 function updateURL() {
     history.replaceState(null, '', `#${currentStep + 1}`);
@@ -30,6 +29,7 @@ nextButtons.forEach(button => {
     button.addEventListener('click', event => {
         event.preventDefault();
         currentStep++;
+        formInstance = document.getElementsByName('step'+currentStep);
         window.VWO = window.VWO || [];
         window.VWO.push(['nls.formAnalysis.markSuccess', formInstance, 1]);
         if (currentStep >= forms.length) {
